a95e9744c203f4a5077ee132e40f67043697c07f
oxfrd <olaf.chojnacki04@wp.pl>
2025-06-11 19:39:16 +0200

    [delay]: Refactor delay component to use blocking timer.

--- core/ST/STM32L476/halLayerImpl/timer/delay.cpp
+++ core/ST/STM32L476/halLayerImpl/timer/delay.cpp
@@ -3,45 +3,28 @@
 //
 
 #include "delay.h"
-
-#pragma GCC optimize ("O0")
-
-static volatile bool mTimeIsUp{false};
-
-__attribute__((interrupt)) void TIM2_IRQHandler()
-{
-    mTimeIsUp = true;
-
-    TIM2->SR &= ~TIM_SR_UIF;
-}
+#include <cassert>
 
 namespace mcu::delay
 {
     using namespace hal::delay;
 
-    delay::delay(std::shared_ptr<hal::interrupt::ITimeInterrupt> interrupt):
+    delay::delay(std::shared_ptr<hal::timer::ITimer> blocker):
     IDelay()
     {
-        if (interrupt != nullptr)
-        {
-            mInterrupt = interrupt;
-            mInterrupt->disable();
-        }
+        mBlocker = blocker;
+        assert(mBlocker != nullptr);
+        mBlocker->disable();
     }
 
-    // min 25us for now, dont want to waste time for that
     eError delay::delayUs(uint32_t us)
     {
-        mTimeIsUp = false;
-        mInterrupt->setPeriod(us);
-        mInterrupt->enable();
-        while(mTimeIsUp == false){;}
-        
-        return eError::eOk;
+        return mBlocker->setPeriod(us);
     }
 
     eError delay::delayMs(uint32_t ms)
     {
-        return delayUs(ms*1000);
+        return mBlocker->setPeriod(ms*1000+(ms/125*250));
     }
+
 } // delay
--- core/ST/STM32L476/halLayerImpl/timer/delay.h
+++ core/ST/STM32L476/halLayerImpl/timer/delay.h
@@ -7,31 +7,38 @@
 #include "ITimeInterrupt.h"
 #include "stm32l476xx.h"
 
-#include <functional>
-
 namespace mcu::delay 
 {
     using namespace hal::delay;
 
     /**
-     * @brief Class holding functionality of delay. 
+     * @brief Class holding functionality of hardware, blocking delay. 
      */
     class delay : public IDelay
     {
     public:
-        explicit delay(std::shared_ptr<hal::interrupt::ITimeInterrupt> interrupt);
+        explicit delay(std::shared_ptr<hal::timer::ITimer> blocker);
 
+        
+        /**
+         * @brief Block program execution for specified number of us.
+         * @param us block for .. [us]
+         * @return result of execution
+         */
         eError delayUs(uint32_t us) override final;
+        
+        /**
+         * @brief Block program execution for specified number of ms.
+         * @param ms block for .. [ms]
+         * @return result of execution
+         */
         eError delayMs(uint32_t ms) override final;
+
     private:
-        std::shared_ptr<hal::interrupt::ITimeInterrupt> mInterrupt{nullptr};
+
+        /**
+         * @brief Pointer to timer component with blocking functionality. 
+         */
+        std::shared_ptr<hal::timer::ITimer> mBlocker{nullptr};
     };
 }  // namespace mcu::delay
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-    __attribute__((interrupt)) void TIM2_IRQHandler();
-#ifdef __cplusplus
-}
-#endif
