d5932055c63dfd456897da7a67a9f388ad21ba0b
oxfrd <olaf.chojnacki04@wp.pl>
2025-08-21 17:46:29 +0200

    [DS18B20]: - add adressing conditions as parameter - crc count added

--- modules/sensors/DS18B20.cpp
+++ modules/sensors/DS18B20.cpp
@@ -4,12 +4,25 @@
 
 namespace module
 {
-    DS18B20::DS18B20(std::shared_ptr<hal::oneWire::IOneWire> oneWire, std::shared_ptr<hal::delay::IDelay> delay):
+    DS18B20::DS18B20(std::shared_ptr<hal::oneWire::IOneWire> oneWire, std::shared_ptr<hal::delay::IDelay> delay, const uint8_t *adress):
     mOneWire(oneWire),
     mDelay(delay)
     {
         assert(mOneWire != nullptr);
         assert(mDelay != nullptr);
+        
+        if (adress != nullptr)
+        {
+            memcpy(mAdress, adress, cAdressBytesCnt);
+            mAdress[7] = crc8(mAdress, 7);
+            mAdressingMode = cNotSkipAdressing;
+        }
+        else
+        {
+            uint64_t notUsed;
+            getAddress(&notUsed);
+            mAdressingMode = cSkipAdressing;
+        }
     }
 
     eError DS18B20::enable(bool reset)
@@ -19,7 +32,8 @@ namespace module
         {
             return err;
         }
-        return mOneWire->skipAdressing();
+        
+        return mOneWire->send(&mAdressingMode, 1);
     }
 
     eError DS18B20::getTemperature(float* val)
@@ -29,6 +43,8 @@ namespace module
         {
             return err;
         }
+
+        sendAddress();
 
         err = mOneWire->send(&cConvertTemperature, 1);
         if (err != eError::eOk)
@@ -48,29 +64,39 @@ namespace module
             return err;
         }
 
+        sendAddress();
+
         err = mOneWire->send(&cReadScratchpad, 1);
         if (err != eError::eOk)
         {
             return err;
         }
-        
-        std::uint8_t scratchPad[cScratchPadSize];
 
-        err = mOneWire->get(scratchPad, cScratchPadSize);
+        err = mOneWire->get(mScratchPad, cScratchPadSize);
         if (err != eError::eOk)
         {
             return err;
         }
 
-        uint16_t temp{(uint16_t)scratchPad[0] | ((uint16_t)scratchPad[1] << 8)};
+        uint16_t temp{(uint16_t)mScratchPad[0] | ((uint16_t)mScratchPad[1] << 8)};
         *val = static_cast<float>(temp);
         *val /=16;
-
+        if (*val > cMaxValidMeasurement)
+        {
+            return eError::eFail;
+        }
+        
         return eError::eOk;
     }
 
     eError DS18B20::getAddress(uint64_t *address) 
     { 
+        if (mAdressingMode == cNotSkipAdressing)
+        {
+            memcpy(address, mAdress, sizeof(*address));
+            return eError::eOk;
+        }
+
         auto err = mOneWire->reset();
         if (err != eError::eOk)
         {
@@ -83,15 +109,40 @@ namespace module
             return err;
         }
         
-        uint8_t rom_code[8];
-        err = mOneWire->get(rom_code,8);
+        err = mOneWire->get(mAdress, cAdressBytesCnt);
         if (err != eError::eOk)
         {
             return err;
         }
 
-        std::memcpy(address, rom_code, sizeof(address));
+        std::memcpy(address, mAdress, sizeof(*address));
         return err;
+    }
+
+    eError DS18B20::sendAddress()
+    {
+        eError err = eError::eOk;
+        if (mAdressingMode == cNotSkipAdressing)
+        {
+            err = mOneWire->send(mAdress, cAdressBytesCnt);
+        }
+        return err;
+    }
+
+    uint8_t DS18B20::crc8( uint8_t *addr, uint8_t len)
+    {
+        uint8_t crc = 0;
+        
+        while (len--) {
+            uint8_t inbyte = *addr++;
+            for (uint8_t i = 8; i; i--) {
+                uint8_t mix = (crc ^ inbyte) & 0x01;
+                crc >>= 1;
+                if (mix) crc ^= 0x8C;
+                inbyte >>= 1;
+            }
+        }
+        return crc;
     }
 
 }  // namespace module
